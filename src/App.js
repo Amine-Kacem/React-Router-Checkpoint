754e2ad0d26dc932c1701a3c0ad570c54fef86d6
unknown <kacem.amine1112@gmail.com>
2020-09-08 02:37:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { data } from "react";
+import { Link, Route, Switch, Redirect } from "react-router-dom";
+import Category from "./Category";
+import Products from "./Products";
+import Login, { fakeAuth } from "./Login";
+import "./App.css";
 
-function App() {
+const Home = () => (
+  <div>
+    <center>
+      <h2>Home</h2>
+    </center>
+  </div>
+);
+
+const Admin = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        {" "}
+        <h2>Welcome admin </h2>
+      </center>
     </div>
   );
-}
+};
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        fakeAuth.isAuthenticated === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: "/login", state: { from: props.location } }}
+          />
+        )
+      }
+    />
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-light">
+        <ul className="nav navbar-nav">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/category">Category</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+          <li>
+            <Link to="/admin">Admin area</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route exact path="/" component={Home} data={data} />
+        <Route path="/category" component={Category} />
+        <Route path="/login" component={Login} />
+        <PrivateRoute
+          authed={fakeAuth.isAuthenticated}
+          path="/admin"
+          component={Admin}
+        />
+        <Route path="/products" component={Products} />
+      </Switch>
+    </div>
+  );
+};
 
 export default App;
